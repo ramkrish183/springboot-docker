2cf56f64ed6570e1d20187637b941c0ba84201c5
RameshJ <rameshkrishnan183@gmail.com>
2023-07-04 12:54:42 -0400

    Initial version

---
+++ springboot-svc/src/test/java/com/rameshj/SpringbootSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rameshj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
